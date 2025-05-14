330fe9fcfe4e703365745bc24437dae9c91f6012
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-14 13:24:09 +0300

    this is first example usestate hooks one

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import UseState1 from "./components/usestate1"
+
+
+function App() {
+
+
+  return (
+    <>
+      <UseState1 />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/usestate1.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const UseState1 = () => {
+
+
+
+
+
+    return (
+        <div className='h-screen flex justify-center items-center bg-gray-100'>
+
+        </div>
+    );
+}
+export default UseState1;
