a80ebae6b7f3edfe5890d3625828601c3ad2dd77
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-19 11:48:31 +0300

    this is example 23 of usestate with react Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import UseState22 from "./UseSate/usestate22"
+import UseState23 from "./UseSate/usestate23"
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <UseState22 />
+      <UseState23 />
 
     </>
   )
