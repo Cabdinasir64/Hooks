3a4fd1acb68a35b037b016d0db17dcf3d3c75920
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-21 17:27:19 +0300

    this is example 11 of useEffect with react Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import UseEffect10 from "./UseEffect/useeffect10"
+import UseEffect11 from "./UseEffect/useeffect11"
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <UseEffect10 />
+      <UseEffect11 />
 
     </>
   )
