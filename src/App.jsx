acf0b6ed16303220517c3e8617c5d1ebbdd15217
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-22 17:24:14 +0300

    i remove router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UseEffect13 from "./UseEffect/useeffect13";
 import UseEffect14 from "./UseEffect/useeffect14";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<UseEffect13 />} />
-        <Route path="/course" element={<UseEffect14 />} />
-      </Routes>
-    </BrowserRouter>
+    <UseEffect14 />
   );
 }
 
