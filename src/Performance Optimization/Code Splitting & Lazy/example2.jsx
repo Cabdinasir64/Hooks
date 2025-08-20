a2cbdd8d772b30a99906d9ba57ecd80df436f610
Cabdinasir64 <cabdinasira64@gmail.com>
2025-08-20 10:36:12 +0300

    This is example2 of Code Splitting & Lazy with react perfomance

---
+++ src/Performance Optimization/Code Splitting & Lazy/example2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="mt-6 p-4 border rounded bg-green-100">
+            <h2 className="text-2xl font-bold">About Component</h2>
+            <p>This component was loaded lazily with code splitting!</p>
+        </div>
+    );
+}
