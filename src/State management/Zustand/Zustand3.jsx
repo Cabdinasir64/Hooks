1ebd25ae9fa3d33eb1d7e84b30e58c880b9f4d1e
Cabdinasir64 <cabdinasira64@gmail.com>
2025-08-25 13:28:26 +0300

    this is example3 of Zustand with react State management

---
+++ src/State management/Zustand/Zustand3.jsx
@@ -0,0 +1,26 @@
+import { create } from 'zustand'
+
+export const useTodoStore = create((set) => ({
+    todos: [],
+    addTodo: (text) =>
+        set((state) => ({
+            todos: [...state.todos, { id: Date.now(), text, completed: false }]
+        })),
+    toggleTodo: (id) =>
+        set((state) => ({
+            todos: state.todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        })),
+    editTodo: (id, newText) =>
+        set((state) => ({
+            todos: state.todos.map((todo) =>
+                todo.id === id ? { ...todo, text: newText } : todo
+            )
+        })),
+    removeTodo: (id) =>
+        set((state) => ({
+            todos: state.todos.filter((todo) => todo.id !== id)
+        })),
+    clearAll: () => set({ todos: [] })
+}))
